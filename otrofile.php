f4f0244eeb60b47cd875b50dd15778934177e0de
Ivan Moreno <cmoreno@swapps.com>
2025-07-22 15:24:49 -0500

    test codium

--- otrofile.php
+++ otrofile.php
@@ -1,3 +1,16 @@
 <?php
-addslashes
+/**
+ * Demonstrates the use of addslashes in PHP.
+ */
+
+function escape_input($input) {
+    return addslashes($input);
+}
+
+// Example usage:
+$user_input = "O'Reilly \"Books\" \\ Test";
+$escaped_input = escape_input($user_input);
+
+echo "Original: $user_input\n";
+echo "Escaped: $escaped_input\n";
 ?>
